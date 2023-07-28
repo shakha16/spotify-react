bc6d31f4f5b4c6f06d603c35a1ca44cc3bab0f5c
Daler <dsarifkulo021@gmail.com>
2023-07-28 17:58:03 +0500

    fff

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
 			<div className="wrapper">
 				<div className="w-[200px] bg-red-500 h-[200px]"></div>
 				<div className="w-[200px] bg-blue-500 h-[200px]"></div>
+				
 			</div>	
 		</>
 	);
