40b35bc54bbd995089d56a1159cf0dcb31623d95
Daler <dsarifkulo021@gmail.com>
2023-07-28 17:47:38 +0500

    fff

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+	return (
+		<>
+			<div className="wrapper">
+				<div className="w-[200px] bg-red-500 h-[200px]"></div>
+				<div className="w-[200px] bg-blue-500 h-[200px]"></div>
+			</div>	
+		</>
+	);
+}
+
+export default App;
